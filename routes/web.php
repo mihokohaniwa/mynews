7e6b947c599ec33750013f04137c15beac7db14d
mihokohaniwa <styraxtonkinensis@gmail.com>
2019-05-29 13:26:54 +0000

    laravel12 確認

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::post('news/create', 'Admin\NewsController@create');
 });
 
 
